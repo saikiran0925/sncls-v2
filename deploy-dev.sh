c57e51a0b53f9ed15353a56f4e801da2f4439ce7
Sai Kiran Bade <saikiranbade2019@gmail.com>
2026-03-11 22:16:12 +0530

    Update script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-SERVER_USER="sncls-dev"
-SERVER_IP="64.227.173.97"
-DEPLOY_PATH="/home/$SERVER_USER/deploy/dev-sncls"
+SERVER_USER="catalyst"
+SERVER_IP="159.65.145.78"
+DEPLOY_PATH="/home/$SERVER_USER/deploy/sncls"
 LOCAL_BUILD_PATH="dist"
 
 echo "Building the React app..."
 npm run build
 
 echo "Transferring build to server..."
-scp -r $LOCAL_BUILD_PATH $SERVER_USER@$SERVER_IP:$DEPLOY_PATH/
+scp -r $LOCAL_BUILD_PATH $SERVER_USER:$DEPLOY_PATH/
 
-ssh $SERVER_USER@$SERVER_IP <<EOF
+ssh $SERVER_USER <<EOF
     echo "Restarting Nginx..."
     sudo systemctl restart nginx
     echo "Deployment completed!"
